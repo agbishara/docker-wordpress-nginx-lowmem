dcb92e456542651bc595090e3f1a78a2194f79e5
michaelczerwinski <mczerwinski@equalexperts.com>
2015-05-25 13:24:09 +0100

    Rework initial commit

--- Dockerfile
+++ Dockerfile
@@ -1,29 +1,34 @@
 FROM ubuntu:14.04
-MAINTAINER Eugene Ware <eugene@noblesamurai.com>
+MAINTAINER fluential <fluential@teqnix.net>
 
 # Keep upstart from complaining
 RUN dpkg-divert --local --rename --add /sbin/initctl
 RUN ln -sf /bin/true /sbin/initctl
-
 # Let the conatiner know that there is no tty
 ENV DEBIAN_FRONTEND noninteractive
+ADD ./nginx-ppa.list /etc/apt/sources.list.d/nginx-ppa.list
+RUN apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 4AA5C398D43A36E6
 
 RUN apt-get update
 RUN apt-get -y upgrade
+RUN apt-get update
 
 # Basic Requirements
-RUN apt-get -y install mysql-server mysql-client nginx php5-fpm php5-mysql php-apc pwgen python-setuptools curl git unzip
+RUN apt-get -y install mysql-server mysql-client php5-fpm php5-mysql php-apc pwgen python-setuptools curl git unzip nginx-custom
 
 # Wordpress Requirements
 RUN apt-get -y install php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-ming php5-ps php5-pspell php5-recode php5-sqlite php5-tidy php5-xmlrpc php5-xsl
 
 # mysql config
 RUN sed -i -e"s/^bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/" /etc/mysql/my.cnf
+ADD ./mysql-low-mem.cnf /etc/mysql/conf.d/mysql-low-mem.cnf.disabled
 
 # nginx config
 RUN sed -i -e"s/keepalive_timeout\s*65/keepalive_timeout 2/" /etc/nginx/nginx.conf
 RUN sed -i -e"s/keepalive_timeout 2/keepalive_timeout 2;\n\tclient_max_body_size 100m/" /etc/nginx/nginx.conf
 RUN echo "daemon off;" >> /etc/nginx/nginx.conf
+RUN ( for url in https://www.cloudflare.com/ips-v4 https://www.cloudflare.com/ips-v6 ;do curl -s $url| xargs -I % echo set_real_ip_from %\;;done;echo real_ip_header CF-Connecting-IP\; ) >/etc/nginx/conf.d/real_ip.conf
+ADD ./nginx-cgi-cache.conf /etc/nginx/conf.d/nginx-cgi-cache.conf
 
 # php-fpm config
 RUN sed -i -e "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g" /etc/php5/fpm/php.ini
@@ -35,16 +40,14 @@ RUN find /etc/php5/cli/conf.d/ -name "*.ini" -exec sed -i -re 's/^(\s*)#(.*)/\1;
 
 # nginx site conf
 ADD ./nginx-site.conf /etc/nginx/sites-available/default
-
 # Supervisor Config
 RUN /usr/bin/easy_install supervisor
 RUN /usr/bin/easy_install supervisor-stdout
 ADD ./supervisord.conf /etc/supervisord.conf
 
 # Install Wordpress
-ADD http://wordpress.org/latest.tar.gz /usr/share/nginx/latest.tar.gz
+ADD https://wordpress.org/latest.tar.gz /usr/share/nginx/latest.tar.gz
 RUN cd /usr/share/nginx/ && tar xvf latest.tar.gz && rm latest.tar.gz
-RUN mv /usr/share/nginx/html/5* /usr/share/nginx/wordpress
 RUN rm -rf /usr/share/nginx/www
 RUN mv /usr/share/nginx/wordpress /usr/share/nginx/www
 RUN chown -R www-data:www-data /usr/share/nginx/www
@@ -56,8 +59,9 @@ RUN chmod 755 /start.sh
 # private expose
 EXPOSE 3306
 EXPOSE 80
+EXPOSE 443
 
-# volume for mysql database and wordpress install
-VOLUME ["/var/lib/mysql", "/usr/share/nginx/www"]
+# volume for configuration and logs
+VOLUME ["/var/lib/mysql", "/usr/share/nginx/www", "/var/log/", "/etc/nginx/"]
 
 CMD ["/bin/bash", "/start.sh"]
--- start.sh
+++ start.sh
@@ -1,41 +1,77 @@
 #!/bin/bash
-if [ ! -f /usr/share/nginx/www/wp-config.php ]; then
+#set -euo pipefail
+
+err() {
+  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $@" >&2
+}
+
+: ${SSL_KEYSIZE:="2048"}
+: ${DH_SIZE:="1024"}
+
+if [[ ! -f /etc/nginx/conf.d/sslbundle.pem ]]; then
+   err "Generating SSL bundle pair..."
+   openssl req -x509 -newkey rsa:${SSL_KEYSIZE} -keyout /etc/nginx/conf.d/sslbundle.pem -out /etc/nginx/conf.d/sslbundle.pem -days 3650 -nodes -subj '/CN=localhost'
+fi
+if [[ ! -f /etc/nginx/conf.d/dhparam.pem ]]; then
+   err "Generating DH param file..."
+   openssl dhparam -outform PEM -out /etc/nginx/conf.d/dhparam.pem ${DH_SIZE}
+fi
+
+if [[ ! -z "$LOW_MEM" ]]; then
+   if [[ ! -f /etc/mysql/conf.d/mysql-low-mem.cnf ]] && [[ ! -f /etc/mysql/conf.d/mysql-low-mem.cnf.disabled ]]; then
+      err "/etc/mysql/conf.d/mysql-low-mem.cnf.disabled missing - caanot enable low memory config"
+      exit 1
+   fi
+   if [[ ! -f /etc/mysql/conf.d/mysql-low-mem.cnf ]]; then
+      mv /etc/mysql/conf.d/mysql-low-mem.cnf.disabled /etc/mysql/conf.d/mysql-low-mem.cnf
+   fi
+fi
+
+if [[ -f /etc/mysql/conf.d/mysql-low-mem.cnf ]]; then
+   err "LOW MEMORY mysql enabled!"
+fi
+
+if [[ ! -f /usr/share/nginx/www/wp-config.php ]]; then
   #mysql has to be started this way as it doesn't work to call from /etc/init.d
-  /usr/bin/mysqld_safe & 
+  /usr/bin/mysqld_safe &
   sleep 10s
   # Here we generate random passwords (thank you pwgen!). The first two are for mysql users, the last batch for random keys in wp-config.php
   WORDPRESS_DB="wordpress"
-  MYSQL_PASSWORD=`pwgen -c -n -1 12`
-  WORDPRESS_PASSWORD=`pwgen -c -n -1 12`
-  #This is so the passwords show up in logs. 
-  echo mysql root password: $MYSQL_PASSWORD
-  echo wordpress password: $WORDPRESS_PASSWORD
+  MYSQL_PASSWORD=$(pwgen -s -1 18)
+  WORDPRESS_PASSWORD=$(pwgen -s -1 18)
+  #This is so the passwords show up in logs.
+  err mysql root password: $MYSQL_PASSWORD
+  err wordpress password: $WORDPRESS_PASSWORD
   echo $MYSQL_PASSWORD > /mysql-root-pw.txt
   echo $WORDPRESS_PASSWORD > /wordpress-db-pw.txt
 
   sed -e "s/database_name_here/$WORDPRESS_DB/
   s/username_here/$WORDPRESS_DB/
   s/password_here/$WORDPRESS_PASSWORD/
-  /'AUTH_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
-  /'SECURE_AUTH_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
-  /'LOGGED_IN_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
-  /'NONCE_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
-  /'AUTH_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/
-  /'SECURE_AUTH_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/
-  /'LOGGED_IN_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/
-  /'NONCE_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/" /usr/share/nginx/www/wp-config-sample.php > /usr/share/nginx/www/wp-config.php
+  /'AUTH_KEY'/s/put your unique phrase here/$(pwgen -s -1 65)/
+  /'SECURE_AUTH_KEY'/s/put your unique phrase here/$(pwgen -s -1 65)/
+  /'LOGGED_IN_KEY'/s/put your unique phrase here/$(pwgen -s -1 65)/
+  /'NONCE_KEY'/s/put your unique phrase here/$(pwgen -s -1 65)/
+  /'AUTH_SALT'/s/put your unique phrase here/$(pwgen -s -1 65)/
+  /'SECURE_AUTH_SALT'/s/put your unique phrase here/$(pwgen -s -1 65)/
+  /'LOGGED_IN_SALT'/s/put your unique phrase here/$(pwgen -s -1 65)/
+  /'NONCE_SALT'/s/put your unique phrase here/$(pwgen -s -1 65)/" /usr/share/nginx/www/wp-config-sample.php > /usr/share/nginx/www/wp-config.php
 
+  PLUGINS="nginx-helper w3-total-cache"
   # Download nginx helper plugin
-  curl -O `curl -i -s https://wordpress.org/plugins/nginx-helper/ | egrep -o "https://downloads.wordpress.org/plugin/[^']+"`
-  unzip -o nginx-helper.*.zip -d /usr/share/nginx/www/wp-content/plugins
-  chown -R www-data:www-data /usr/share/nginx/www/wp-content/plugins/nginx-helper
 
-  # Activate nginx plugin and set up pretty permalink structure once logged in
+  for PLUG in $PLUGINS; do
+     curl -s -O $(curl -i -s https://wordpress.org/plugins/${PLUG}/ | egrep -o "https://downloads.wordpress.org/plugin/[^']+")
+     find . -type f -name "*.zip" -print0 | xargs -0 -I % unzip -q -o % -d /usr/share/nginx/www/wp-content/plugins
+     #unzip -o *.zip -d /usr/share/nginx/www/wp-content/plugins
+     chown -R www-data:www-data /usr/share/nginx/www/wp-content/plugins
+  done
+
+  # Activate nginx plugin
   cat << ENDL >> /usr/share/nginx/www/wp-config.php
 \$plugins = get_option( 'active_plugins' );
 if ( count( \$plugins ) === 0 ) {
   require_once(ABSPATH .'/wp-admin/includes/plugin.php');
-  \$wp_rewrite->set_permalink_structure( '/%postname%/' );
   \$pluginsToActivate = array( 'nginx-helper/nginx-helper.php' );
   foreach ( \$pluginsToActivate as \$plugin ) {
     if ( !in_array( \$plugin, \$plugins ) ) {
@@ -51,6 +87,13 @@ ENDL
   mysql -uroot -p$MYSQL_PASSWORD -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION; FLUSH PRIVILEGES;"
   mysql -uroot -p$MYSQL_PASSWORD -e "CREATE DATABASE wordpress; GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'localhost' IDENTIFIED BY '$WORDPRESS_PASSWORD'; FLUSH PRIVILEGES;"
   killall mysqld
+  cat <<EOF > /root/.my.cnf
+[client]
+user=root
+password="$MYSQL_PASSWORD"
+EOF
+
+err 'Container running...'
 fi
 
 # start all the services
